e3d4e3f66331323c5b8d003afefe68ef0fe967d2
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-25 19:11:43 -0300

    Componente calculator

---
+++ calculadora/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Calculator from "./main/Calculator"
+
+ReactDOM.render(
+    <Calculator/>,
+    document.getElementById('root')
+)
---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react '
+import "./Calculator.css"
+
+
+export default props => {
+        return (
+            <div className="calculator">
+                
+            </div>
+
+        )
+}
